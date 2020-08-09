c31f97655d53707b507781843ca7a31bad1b6481
Michael Li <mli25782@gmail.com>
2020-08-09 16:13:48 -0400

    Updates

---
+++ precache-manifest.d29cc80d2ec09fa5fc4343d1f5067fd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9c5410f02ef094e2efe89ef491070e",
+    "url": "/umdgrades/index.html"
+  },
+  {
+    "revision": "03797d8686a946bffa66",
+    "url": "/umdgrades/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d8b353e6c51c980cb0ac",
+    "url": "/umdgrades/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "03797d8686a946bffa66",
+    "url": "/umdgrades/static/js/2.5c4c9b2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/umdgrades/static/js/2.5c4c9b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b353e6c51c980cb0ac",
+    "url": "/umdgrades/static/js/main.3c92b47e.chunk.js"
+  },
+  {
+    "revision": "7d7de868cf6146df1814",
+    "url": "/umdgrades/static/js/runtime-main.ea8f7532.js"
+  }
+]);
